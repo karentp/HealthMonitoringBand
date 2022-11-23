87b42beee3f0d5b3eb18402f9beae0088c99bcd6
Karen Tovar <tovarparrakaren@gmail.com>
2022-11-23 03:54:57 -0600

    corrigiendo archivos

--- caidas.ino
+++ caidas.ino
@@ -1,19 +1,19 @@
 #include <Wire.h>
 #include <MPU6050.h>
-const int MPU_addr=0x68;  // I2C address of the MPU-6050
+const int MPU_addr=0x68;  
 int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
 float ax=0, ay=0, az=0, gx=0, gy=0, gz=0;
 
-//int data[STORE_SIZE][5]; //array for saving past data
-//byte currentIndex=0; //stores current data array index (0-255)
-boolean fall = false; //stores if a fall has occurred
-boolean trigger1=false; //stores if first trigger (lower threshold) has occurred
-boolean trigger2=false; //stores if second trigger (upper threshold) has occurred
-boolean trigger3=false; //stores if third trigger (orientation change) has occurred
 
-byte trigger1count=0; //stores the counts past since trigger 1 was set true
-byte trigger2count=0; //stores the counts past since trigger 2 was set true
-byte trigger3count=0; //stores the counts past since trigger 3 was set true
+
+boolean fall = false; 
+boolean trigger1=false; 
+boolean trigger2=false; 
+boolean trigger3=false; 
+
+byte trigger1count=0; 
+byte trigger2count=0; 
+byte trigger3count=0; 
 int angleChange=0;
 
 
@@ -22,8 +22,8 @@ pinMode (8, OUTPUT);
 
  Wire.begin();
  Wire.beginTransmission(MPU_addr);
- Wire.write(0x6B);  // PWR_MGMT_1 register
- Wire.write(0);     // set to zero (wakes up the MPU-6050)
+ Wire.write(0x6B);  
+ Wire.write(0);     
  Wire.endTransmission(true);
  Serial.begin(9600);
 
@@ -33,44 +33,44 @@ pinMode (8, OUTPUT);
 void loop(){
 
  mpu_read();
- //2050, 77, 1947 are values for calibration of accelerometer
- // values may be different for you
+ 
+ 
  ax = (AcX-2600)/16384.00;
  ay = (AcY+400)/16384.00;
  az = (AcZ-2000)/16300;
 
 
- //270, 351, 136 for gyroscope
+ 
  gx = (GyX+1490)/131.07;
  gy = (GyY-380)/131.07;
  gz = (GyZ+1090)/131.07;
 
 
-// calculating Amplitute vactor for 3 axis
+
  float Raw_AM = pow(pow(ax,2)+pow(ay,2)+pow(az,2),0.5);
- int AM = Raw_AM * 10;  // as values are within 0 to 1, I multiplied 
-                        // it by for using if else conditions 
+ int AM = Raw_AM * 10;  
+                        
 Serial.println(AM);
 
- //Serial.println(PM);
- //delay(500);
+ 
+ 
 
  if (trigger3==true){
     trigger3count++;
-    //Serial.println(trigger3count);
+    
     if (trigger3count>=8){ 
        angleChange = pow(pow(gx,2)+pow(gy,2)+pow(gz,2),0.5);
-       //delay(10);
-       if ((angleChange>=0) && (angleChange<=10)){ //if orientation changes remains between 0-10 degrees
+       
+       if ((angleChange>=0) && (angleChange<=10)){ 
            fall=true; trigger3=false; trigger3count=0;
              }
-        else{ //user regained normal orientation
+        else{ 
           trigger3=false; trigger3count=0;
           Serial.println("TRIGGER 3 DESACTIVADO");
        }
      }
   }
- if (fall==true){ //in event of a fall detection
+ if (fall==true){ 
    Serial.println("HAY UNA CAIDA");
    digitalWrite(11, LOW);
    delay(20);
@@ -83,21 +83,21 @@ Serial.println(AM);
    trigger2=false;
    trigger1=false;
    delay(1000);
-  // exit(1);
+  
    }
-   if (trigger2count>=6){ //allow 0.5s for orientation change
+   if (trigger2count>=6){ 
    trigger2=false; trigger2count=0;
    Serial.println("TRIGGER 2 DESACTIVADO");
    }
- if (trigger1count>=12){ //allow 0.5s for AM to break upper threshold
+ if (trigger1count>=12){ 
    trigger1=false; trigger1count=0;
    Serial.println("TRIGGER 1 DESACTIVADO");
    }
  if (trigger2==true){
    trigger2count++;
-   //angleChange=acos(((double)x*(double)bx+(double)y*(double)by+(double)z*(double)bz)/(double)AM/(double)BM);
+   
    angleChange = pow(pow(gx,2)+pow(gy,2)+pow(gz,2),0.5);
-   if (angleChange>=30 && angleChange<=400){ //if orientation changes by between 80-100 degrees
+   if (angleChange>=30 && angleChange<=400){ 
       Serial.println("MOVIMIENTO CANDIDATO A CAERSE");   
      trigger3=true; trigger2=false; trigger2count=0;
        }
@@ -105,29 +105,29 @@ Serial.println(AM);
  if (trigger1==true){
    trigger1count++;
    Serial.println(AM);
-   if (AM>=15){ //if AM breaks upper threshold (3g)
+   if (AM>=15){ 
      trigger2=true;
      trigger1=false; trigger1count=0;
      }
    }
- if (AM>=11 && trigger2==false){ //if AM breaks lower threshold (0.4g)
+ if (AM>=11 && trigger2==false){ 
    trigger1=true;
 
    }
-//It appears that delay is needed in order not to clog the port
+
  delay(100);
 }
 
 void mpu_read(){
  Wire.beginTransmission(MPU_addr);
- Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+ Wire.write(0x3B);  
  Wire.endTransmission(false);
- Wire.requestFrom(MPU_addr,14,true);  // request a total of 14 registers
- AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
- AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
- AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
- Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
- GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
- GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
- GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+ Wire.requestFrom(MPU_addr,14,true);  
+ AcX=Wire.read()<<8|Wire.read();  
+ AcY=Wire.read()<<8|Wire.read();  
+ AcZ=Wire.read()<<8|Wire.read();  
+ Tmp=Wire.read()<<8|Wire.read();  
+ GyX=Wire.read()<<8|Wire.read();  
+ GyY=Wire.read()<<8|Wire.read();  
+ GyZ=Wire.read()<<8|Wire.read();  
  }
--- ritmocardiaco.ino
+++ ritmocardiaco.ino
@@ -1,27 +1,27 @@
-#define USE_ARDUINO_INTERRUPTS true // Set-up low-level interrupts for most acurate BPM math.
-#include <PulseSensorPlayground.h> // Includes the PulseSensorPlayground Library.
+#define USE_ARDUINO_INTERRUPTS true 
+#include <PulseSensorPlayground.h> 
 #include <LiquidCrystal_I2C.h>
 LiquidCrystal_I2C lcd(0x27, 2,1,0,4,5,6,7,3, POSITIVE);
  
-// Variables
-const int PulseWire = 0; // PulseSensor PURPLE WIRE connected to ANALOG PIN 0
-const int LED13 = 13; // The on-board Arduino LED, close to PIN 13.
-int Threshold = 550; // Determine which Signal to "count as a beat" and which to ignore.
-// Use the "Gettting Started Project" to fine-tune Threshold Value beyond default setting.
-// Otherwise leave the default "550" value.
+
+const int PulseWire = 0; 
+const int LED13 = 13; 
+int Threshold = 550; 
+
+
  
-PulseSensorPlayground pulseSensor; // Creates an instance of the PulseSensorPlayground object called "pulseSensor"
+PulseSensorPlayground pulseSensor; 
 void setup() {
  
-Serial.begin(9600); // For Serial Monitor
+Serial.begin(9600); 
 lcd.begin(16,2);
  
-// Configure the PulseSensor object, by assigning our variables to it.
+
 pulseSensor.analogInput(PulseWire);
-pulseSensor.blinkOnPulse(LED13); //auto-magically blink Arduino's LED with heartbeat.
+pulseSensor.blinkOnPulse(LED13); 
 pulseSensor.setThreshold(Threshold);
  
-// Double-check the "pulseSensor" object was created and "began" seeing a signal.
+
 if (pulseSensor.begin()) {
 lcd.print(" Monitoreo de Ritmo Cardiaco");
  
@@ -30,16 +30,16 @@ lcd.print(" Monitoreo de Ritmo Cardiaco");
  
 void loop() {
  
-int myBPM = pulseSensor.getBeatsPerMinute(); // Calls function on our pulseSensor object that returns BPM as an "int".
-// "myBPM" hold this BPM value now.
-if (pulseSensor.sawStartOfBeat()) { // Constantly test to see if "a beat happened".
-Serial.println("♥ Pulso Cardiaco detectado ! "); // If test is "true", print a message "a heartbeat happened".
-Serial.print("BPM: "); // Print phrase "BPM: "
-Serial.println(myBPM); // Print the value inside of myBPM.
+int myBPM = pulseSensor.getBeatsPerMinute(); 
 
-lcd.print("Pulso Cardiaco detectado !"); // If test is "true", print a message "a heartbeat happened".
-lcd.print("BPM: "); // Print phrase "BPM: "
+if (pulseSensor.sawStartOfBeat()) { 
+Serial.println("♥ Pulso Cardiaco detectado ! "); 
+Serial.print("BPM: "); 
+Serial.println(myBPM); 
+
+lcd.print("Pulso Cardiaco detectado !"); 
+lcd.print("BPM: "); 
 lcd.print(myBPM);
 }
-delay(20); // considered best practice in a simple sketch.
+delay(20); 
 }
