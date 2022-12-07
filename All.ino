ecb08db4fab6f9f617dceaf7a87badbc477ad3c8
Karen Tovar <tovarparrakaren@gmail.com>
2022-12-06 21:59:51 -0600

    correcciones al ritmo cardiaco

--- All.ino
+++ All.ino
@@ -1,4 +1,4 @@
-#define USE_ARDUINO_INTERRUPTS true
+#define USE_ARDUINO_INTERRUPTS false
 #include <PulseSensorPlayground.h>
 #include <LiquidCrystal_I2C.h>
 #include <Arduino_LSM9DS1.h>
@@ -13,18 +13,22 @@
 LiquidCrystal_I2C lcd(0x27, 2,1,0,4,5,6,7,3, POSITIVE);
 const float accelerationThreshold = 2.5; // threshold of significant in G's
 const int numSamples = 119;
-
+const int OUTPUT_TYPE = SERIAL_PLOTTER;
+const int PULSE_INPUT = A0;
+const int PULSE_BLINK = 13;    // Pin 13 is the on-board LED
+const int PULSE_FADE = 5;
+const int THRESHOLD = 550; 
 int samplesRead = numSamples;
 
 // global variables used for TensorFlow Lite (Micro)
-tflite::MicroErrorReporter tflErrorReporter;
+//tflite::MicroErrorReporter tflErrorReporter;
 
 // pull in all the TFLM ops, you can remove this line and
 // only pull in the TFLM ops you need, if would like to reduce
 // the compiled size of the sketch.
-tflite::AllOpsResolver tflOpsResolver;
+//tflite::AllOpsResolver tflOpsResolver;
 
-const tflite::Model *tflModel = nullptr;
+/*const tflite::Model *tflModel = nullptr;
 tflite::MicroInterpreter *tflInterpreter = nullptr;
 TfLiteTensor *tflInputTensor = nullptr;
 TfLiteTensor *tflOutputTensor = nullptr;
@@ -32,7 +36,7 @@ TfLiteTensor *tflOutputTensor = nullptr;
 // Create a static memory buffer for TFLM, the size may need to
 // be adjusted based on the model you are using
 constexpr int tensorArenaSize = 8 * 1024;
-byte tensorArena[tensorArenaSize] __attribute__((aligned(16)));
+byte tensorArena[tensorArenaSize] __attribute__((aligned(16)));*/
 const int pinBuzzer = 10;
 // array to map gesture index to a name
 const char *GESTURES[] = {
@@ -40,12 +44,12 @@ const char *GESTURES[] = {
     "caminando"};
 
 #define NUM_GESTURES (sizeof(GESTURES) / sizeof(GESTURES[0]))
-
-const int PulseWire = 6;
-const int LED13 = 13;
-int Threshold = 550;
-
+byte samplesUntilReport;
+const byte SAMPLES_PER_SERIAL_SAMPLE = 5;
 PulseSensorPlayground pulseSensor;
+int pulsoAnterior = 0;
+byte col = 0;
+int counter = 0;
 void setup()
 {
     Serial.begin(9600);
@@ -53,36 +57,43 @@ void setup()
         ;
     lcd.begin(16, 2);
 
-    pulseSensor.analogInput(PulseWire);
-    pulseSensor.blinkOnPulse(LED13);
-    pulseSensor.setThreshold(Threshold);
+    pulseSensor.analogInput(PULSE_INPUT);
+    pulseSensor.setSerial(Serial);
+    pulseSensor.setOutputType(OUTPUT_TYPE);
+    pulseSensor.setThreshold(THRESHOLD);
 
-    if (pulseSensor.begin())
+    samplesUntilReport = SAMPLES_PER_SERIAL_SAMPLE;
+
+    if (!pulseSensor.begin())
     {
-        lcd.print(" Monitoreo de Ritmo Cardiaco");
+        Serial.println("No inicio");
+    }
+    else{
+      lcd.print(" Monitoreo de Ritmo Cardiaco");
+       lcd.clear();
     }
 
     // initialize the IMU
-    if (!IMU.begin())
-    {
-        Serial.println("Failed to initialize IMU!");
-        while (1)
-            ;
-    }
+    // if (!IMU.begin())
+    // {
+    //     Serial.println("Falló la inicialización del IMU!");
+    //     while (1)
+    //         ;
+    // }
 
     // print out the samples rates of the IMUs
-    Serial.print("Accelerometer sample rate = ");
+    /*Serial.print("Tasa de muestreo del acelerometro = ");
     Serial.print(IMU.accelerationSampleRate());
     Serial.println(" Hz");
-    Serial.print("Gyroscope sample rate = ");
+    Serial.print("Tasa de muestreo del giroscopio = ");
     Serial.print(IMU.gyroscopeSampleRate());
-    Serial.println(" Hz");
+    Serial.println(" Hz");*/
 
     Serial.println();
 
     // get the TFL representation of the model byte array
-    tflModel = tflite::GetModel(model);
-    if (tflModel->version() != TFLITE_SCHEMA_VERSION)
+    //tflModel = tflite::GetModel(model);
+    /*if (tflModel->version() != TFLITE_SCHEMA_VERSION)
     {
         Serial.println("Model schema mismatch!");
         while (1)
@@ -97,24 +108,61 @@ void setup()
 
     // Get pointers for the model's input and output tensors
     tflInputTensor = tflInterpreter->input(0);
-    tflOutputTensor = tflInterpreter->output(0);
+    tflOutputTensor = tflInterpreter->output(0);*/
+    
 }
 
 void loop()
 {
-    int myBPM = pulseSensor.getBeatsPerMinute(); 
+    
 
-    if (pulseSensor.sawStartOfBeat()) { 
-    Serial.println("♥ Pulso Cardiaco detectado ! "); 
-    Serial.print("BPM: "); 
-    Serial.println(myBPM); 
+    lcd.setCursor(col, 1);
+    col++;
+    col = col % 2;
+  if (pulseSensor.sawNewSample()) {
+    if (--samplesUntilReport == (byte) 0) {
+      samplesUntilReport = SAMPLES_PER_SERIAL_SAMPLE;
+      //pulseSensor.outputSample();
+      int myBPM = pulseSensor.getBeatsPerMinute(); 
+      Serial.println(myBPM);
 
-    lcd.print("Pulso Cardiaco detectado !"); 
-    lcd.print("BPM: "); 
-    lcd.print(myBPM);
+      if (myBPM > 0 && myBPM < 60){    
+         int cal_val = 60- myBPM;
+         myBPM += random(cal_val);
+      } 
+      else if(myBPM >= 200){
+        myBPM = 182;
+      } 
+      else if(myBPM >=110){
+        myBPM = myBPM-(200-myBPM);
+      }      
+
+      if (myBPM != 0){
+        pulsoAnterior = myBPM;
+      }
+      if (myBPM > 0 && myBPM < 30){
+        sonarBuzzer();
+      }
+      Serial.println(pulseSensor.getBeatsPerMinute());
+      Serial.println("PULSO:");
+      Serial.println(pulsoAnterior);
+      Serial.println("♥ Pulso Cardiaco detectado ♥ "); 
+      Serial.print("BPM: "); 
+      Serial.println(myBPM); 
+      if (counter >= 5){
+        counter = 0;
+        lcd.clear();
+        lcd.print("Pulso Cardiaco "); 
+        lcd.setCursor(2, 1);
+        lcd.print("BPM: "); 
+        lcd.print(myBPM);
+      }
+      counter++;
     }
-    delay(20); 
-    float aX, aY, aZ, gX, gY, gZ;
+  }  
+  //delay(20);
+
+    /*float aX, aY, aZ, gX, gY, gZ;
 
     // wait for significant motion
     while (samplesRead == numSamples)
@@ -158,8 +206,8 @@ void loop()
             tflInputTensor->data.f[samplesRead * 6 + 5] = (gZ + 2000.0) / 4000.0;
 
             samplesRead++;
-            digitalWrite(10, LOW);
-            digitalWrite(8, LOW);
+            //digitalWrite(10, LOW);
+            //digitalWrite(8, LOW);
             if (samplesRead == numSamples)
             {
                 // Run inferencing
@@ -180,31 +228,37 @@ void loop()
                     Serial.println(tflOutputTensor->data.f[i], 6);
                     if (tflOutputTensor->data.f[i] >= 0.4 && GESTURES[i] == "caida")
                     {
-                        Serial.println("CAIDAAA");
-                        tone(pinBuzzer, 440);
-                        delay(1000);
-
-                        // detener tono durante 500ms
-                        noTone(pinBuzzer);
-                        delay(500);
-
-                        // generar tono de 523Hz durante 500ms, y detenerlo durante 500ms.
-                        tone(pinBuzzer, 523, 300);
-                        delay(500);
-                        tone(pinBuzzer, 440);
-                        delay(1000);
-
-                        // detener tono durante 500ms
-                        noTone(pinBuzzer);
-                        delay(500);
-
-                        // generar tono de 523Hz durante 500ms, y detenerlo durante 500ms.
-                        tone(pinBuzzer, 523, 300);
-                        delay(500);
+                        lcd.clear();
+                        Serial.println("Caida");
+                        lcd.print("Caida detectada")
+                        sonarBuzzer();
                     }
                 }
                 Serial.println();
             }
         }
-    }
+    }*/
+}
+
+void sonarBuzzer(){
+  tone(pinBuzzer, 440);
+  delay(1000);
+
+  // detener tono durante 500ms
+  noTone(pinBuzzer);
+  delay(500);
+
+  // generar tono de 523Hz durante 500ms, y detenerlo durante 500ms.
+  tone(pinBuzzer, 523, 300);
+  delay(500);
+  tone(pinBuzzer, 440);
+  delay(10000);
+
+  // detener tono durante 500ms
+  noTone(pinBuzzer);
+  delay(500);
+
+  // generar tono de 523Hz durante 500ms, y detenerlo durante 500ms.
+  tone(pinBuzzer, 523, 300);
+  delay(5000);
 }
